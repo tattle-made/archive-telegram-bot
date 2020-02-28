a8c899ea03710cf5566198c2214a17a2ef477ef1
denny <denny.george90@gmail.com>
2020-02-28 16:49:40 +0530

    (feat) revert to polling

--- prototype.py
+++ prototype.py
@@ -308,12 +308,12 @@ dispatcher.add_handler(restart_handler)
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(storing_data_handler)
 
-updater.start_webhook(listen="0.0.0.0",
-	port=PORT,
-	url_path=TOKEN)
+# updater.start_webhook(listen="0.0.0.0",
+# 	port=PORT,
+# 	url_path=TOKEN)
 
-updater.bot.set_webhook("https://services-server-dev.tattle.co.in/tb/" + TOKEN)
-# updater.start_polling()
+# updater.bot.set_webhook("https://services-server-dev.tattle.co.in/tb/" + TOKEN)
+updater.start_polling()
 updater.idle()	
 
 
